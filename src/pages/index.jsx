1ca83447ef3686715f43569a9e33999d2513f487
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-10-02 19:23:26 +0200

    broke down how state is passed down

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Meta from "@components/Meta";
+import { useState } from "react";
 
 const json = {
   _id: "1",
@@ -28,14 +29,21 @@ function Company({ _id, name, employees, onChange = () => {} }) {
           className="text-gray-900 p-2"
           type="text"
           value={name}
-          onChange={(e) => onChange(e.target.value)}
+          onChange={(e) =>
+            onChange((prev) => ({
+              ...prev,
+              name: e.target.value,
+            }))
+          }
         />
       </div>
     </div>
   );
 }
 
-export default function Home() {
+export default function Home({ company }) {
+  // if prop and state are the same (company), underscore the inner
+  const [_company, setCompany] = useState(company);
   return (
     <>
       <Meta
@@ -47,8 +55,34 @@ export default function Home() {
       <div className="container mx-auto min-h-screen p-20">
         <h1 className="p-4 text-4xl font-semibold">S.O.L.I.D.</h1>
         <div className="border border-gray-600 mb-8" />
-        <Company {...json} />
+        <Company {..._company} onChange={(company) => setCompany(company)} />
       </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  return {
+    // props is an object
+
+    props: {
+      company: {
+        _id: "1",
+        name: "company 1",
+        employees: [
+          {
+            name: "employee 1",
+            tags: [
+              {
+                label: "label 1",
+              },
+              {
+                label: "label 2",
+              },
+            ],
+          },
+        ],
+      },
+    },
+  };
 }
